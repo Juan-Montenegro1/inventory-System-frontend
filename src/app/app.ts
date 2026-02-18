7a45f24d3d035ccc02f7a3256e2d30ef3a0a3b3c
Juan Manuel Montenegro <juanmonte2004@gmail.com>
2026-02-18 12:48:26 -0500

    feat: create component producto-listado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductoListado } from "./producto-listado/producto-listado.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductoListado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
