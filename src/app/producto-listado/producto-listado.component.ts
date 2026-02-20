61e79c98d4733c15530156905c0004a7c7aa977c
Juan Manuel Montenegro <juanmonte2004@gmail.com>
2026-02-19 19:56:03 -0500

    feat: creacion de producto-services

--- src/app/producto-listado/producto-listado.component.ts
+++ src/app/producto-listado/producto-listado.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-listado',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './producto-listado.component.html',
 })
 export class ProductoListado {
+  productos!: Producto[];
 
+  private productoService = inject(ProductoService);
+
+  ngOnInit(){
+    //CARGAR PRODUCTOS
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos(): void{
+    this.productoService.obtenerProductosLista().subscribe(
+      {
+        next: (datos) => {
+          this.productos = datos;
+        },
+        error: (error) => {
+          console.log("Error al obtener los productos: " + error);
+        }
+      }
+    );
+  }
 }
